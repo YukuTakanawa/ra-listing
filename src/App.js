bd614cde329b31c0773dbef52eeb95a1c65163bc
Takanawa Yuku <yukutakanawa@gmail.com>
2023-07-04 14:42:33 +0300

    homework 2

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import Listing from './components/Listing';
+import etsy from './data/etsy.json';
+
+function App() {
+  return <Listing items={etsy} />;
+}
+
+export default App;
