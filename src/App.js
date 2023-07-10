e24419cf198a4502875d71f8e64462d5e0caf321
Takanawa Yuku <yukutakanawa@gmail.com>
2023-07-10 11:41:38 +0300

    edit hw

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-import Listing from './components/Listing';
-import etsy from './data/etsy.json';
-
-function App() {
-  return <Listing items={etsy} />;
-}
-
-export default App;
